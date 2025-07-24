e9f6625d265f5311a35d9c348d8b0f8c94078fc1
Andres <Andres-T7@users.noreply.github.com>
2025-07-23 21:32:38 -0400

    mejora en la descripcion de graficos

--- app.py
+++ app.py
@@ -2,32 +2,70 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.header('Visualizador de Datos Mercado de Vehiculos')
+st.header('Análisis Exploratorio de Vehículos')
 
 car_data = pd.read_csv("vehicles_us.csv")  # leer los datos
-hist_button = st.button('Construir histograma')  # crear un botón
+hist_button = st.button('Generar Histograma')  # crear un botón
 
 if hist_button:  # al hacer clic en el botón
     # escribir un mensaje
-    st.write('Creación de un histograma para Distribución de precios de los Vehiculos')
+    st.write('Permite visualizar cómo están distribuidos los precios de los vehículos')
 
     # crear un histograma
     fig = px.histogram(car_data, x="price", nbins=50,
-                       title="Distribución de precios")
+                       title="Distribución de precios",
+                       labels={
+                           "price": "Precio del vehículo (USD)",
+                           "count": "Cantidad de vehículos"})
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
 
-hist_button_2 = st.button('Construir Diagrama de Dispersion')  # crear un botón
+hist_button_2 = st.button('Diagrama de Dispersion')  # crear un botón
 
 if hist_button_2:  # al hacer clic en el botón
     # escribir un mensaje
-    st.write('Creación de un Diagrama de Dispersion para ver Precio vs Año del modelo')
+    st.write('Muestra la relación entre el año del modelo y el precio del vehículo.')
 
     # crear un diagrama de Dispersion
     fig = px.scatter(car_data, x="model_year", y="price",
-                     title="Precio vs Año del modelo", opacity=0.6)
+                     title="Precio vs Año del modelo", opacity=0.6, labels={
+                         "price": "Precio del vehículo (USD)",
+                         "model_year": "Año del modelo"})
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+hist_button_3 = st.button('Boxplot')  # crear un botón
+
+if hist_button_3:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Compara las distribuciones de precio según el tipo de combustible')
+
+# Boxplot por tipo de combustible
+fig = px.box(car_data, x="fuel", y="price",
+             title="Precio por tipo de combustible", labels={
+                         "price": "Precio del Combustible(USD)",
+                         "fuel": "Tipo de Combustible"})
+
+# mostrar un gráfico Plotly interactivo
+st.plotly_chart(fig, use_container_width=True)
+
+hist_button_4 = st.button('Construir grafico de barras')  # crear un botón
+
+if hist_button_4:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Es útil para entender la composición del inventario y qué categorías dominan el dataset.')
+
+# Gráfico de barras: número de vehículos por tipo
+df_counts = car_data['type'].value_counts().reset_index()
+# Renombrar columnas para mayor claridad
+df_counts.columns = ['Tipo de vehículo', 'Cantidad']
+
+fig = px.bar(df_counts, x='Tipo de vehículo', y='Cantidad',
+             title='Número de vehículos por tipo')
+
+# mostrar un gráfico Plotly interactivo
+st.plotly_chart(fig, use_container_width=True)
